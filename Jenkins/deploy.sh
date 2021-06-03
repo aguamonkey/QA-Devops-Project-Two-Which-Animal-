074843ba01e1a7a412e647f7559725ff97834afa
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 11:30:04 +0000

    Trying to edit the deploy.sh to run create.py

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,5 +2,6 @@ scp docker-compose.yaml docker:
 ssh docker << EOF
 #export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
+docker exec which_animal_am_i_server python3 create.py
 docker stack deploy --compose-file docker-compose.yaml app
 EOF
