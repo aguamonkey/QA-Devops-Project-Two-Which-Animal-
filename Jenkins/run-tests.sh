ee044fc99dd1515470ce7b42651b5bdbc4a929ab
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 10:07:53 +0000

    Jenkins attempt simple error fix two

--- Jenkins/run-tests.sh
+++ Jenkins/run-tests.sh
@@ -6,7 +6,7 @@ python3 -m venv venv
 source venv/bin/activate
 
 #Install pytest
-pip3 install -r service-1/requirements.txt
+pip3 install -r service1/requirements.txt
 
 # run pytest will need for loop at some point
 python3 -m pytest service1 --junitxml=junit/test-results.xml --cov=application --cov-report=xml --cov-report=html --cov-report term-missing
