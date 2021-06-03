4c6e18504cda8b19f94b494a3ab408f37ed3c98d
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 12:13:17 +0000

    changed sudo visudo

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,7 +2,6 @@ scp docker-compose.yaml docker:
 ssh docker << EOF
 #export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
-ssh -oStrictHostKeyChecking=no host
 docker exec which_animal_am_i_server python3 create.py
 docker stack deploy --compose-file docker-compose.yaml app
 EOF
