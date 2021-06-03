0f57838b78b3f96fe46d93452ad93c9bdefcb943
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 15:17:36 +0000

    ansible coming together

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,7 +1,8 @@
 scp docker-compose.yaml docker:
 ssh docker << EOF
-#export SECRET_KEY=${SECRET_KEY}
+export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
-docker exec which_animal_am_i_server python3 create.py
 docker stack deploy --compose-file docker-compose.yaml app
+docker exec which_animal_am_i_server python3 create.py
+
 EOF
