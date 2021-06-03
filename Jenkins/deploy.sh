3786dd92814a93024dc6bb65bf188db219ba3f66
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 11:03:08 +0000

    deploy.sh secret key removed

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,6 +1,5 @@
 scp docker-compose.yaml docker:
 ssh docker << EOF
-export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
 docker stack deploy --compose-file docker-compose.yaml app
 EOF
