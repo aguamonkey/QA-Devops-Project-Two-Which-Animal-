185a4f469dc55ae615adc4cab642721089c6a458
aguamonkey <joshuabrowne1@icloud.com>
2021-06-03 11:43:07 +0000

    Testing the ssh in deploy.sh

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,6 +2,7 @@ scp docker-compose.yaml docker:
 ssh docker << EOF
 #export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
+ssh -oStrictHostKeyChecking=no host
 docker exec which_animal_am_i_server python3 create.py
 docker stack deploy --compose-file docker-compose.yaml app
 EOF
