be17fc68322181a9920336f197bc97d2d67512f8
aguamonkey <joshuabrowne1@icloud.com>
2021-06-04 12:22:32 +0000

    deploy.sh edited

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -1,5 +1,5 @@
-scp docker-compose.yaml docker:
-ssh docker << EOF
+scp docker-compose.yaml docker-manager:
+ssh docker-manager << EOF
 export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
 docker stack deploy --compose-file docker-compose.yaml app
