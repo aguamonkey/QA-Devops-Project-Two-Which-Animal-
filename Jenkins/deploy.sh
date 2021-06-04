f0884fe0bbd1101e20b7cea56aa57e61dfff35e8
aguamonkey <joshuabrowne1@icloud.com>
2021-06-04 15:02:39 +0000

    should finally work deploy.sh

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -4,5 +4,4 @@ export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
 docker stack deploy --compose-file docker-compose.yaml which_animal_am_i_server
 docker exec which_animal_am_i_server python create.py
-
 EOF
