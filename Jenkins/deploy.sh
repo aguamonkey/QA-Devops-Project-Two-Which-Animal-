a7ad572d4262c28f5d2368860563a4ee7b37fee0
aguamonkey <joshuabrowne1@icloud.com>
2021-06-04 15:16:40 +0000

    deploy.sh

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,5 +2,5 @@ scp docker-compose.yaml docker-manager:
 ssh docker-manager << EOF
 export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml which_animal_am_i_server
+docker stack deploy --compose-file docker-compose.yaml
 EOF
