4fe51a01c2ac305dd54815dde5467ea06564b790
aguamonkey <joshuabrowne1@icloud.com>
2021-06-04 14:44:59 +0000

    deploy.sh added server to stack

--- Jenkins/deploy.sh
+++ Jenkins/deploy.sh
@@ -2,7 +2,7 @@ scp docker-compose.yaml docker-manager:
 ssh docker-manager << EOF
 export SECRET_KEY=${SECRET_KEY}
 export DATABASE_URI=${DATABASE_URI}
-docker stack deploy --compose-file docker-compose.yaml app
+docker stack deploy --compose-file docker-compose.yaml which_animal_am_i_server
 docker exec which_animal_am_i_server python create.py
 
 EOF
