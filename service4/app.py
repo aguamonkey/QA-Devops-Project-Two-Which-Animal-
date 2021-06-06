33f6ee3a15f4083f843880a7c235f4ccf04943c0
aguamonkey <joshuabrowne1@icloud.com>
2021-06-06 05:06:17 +0000

    Tests all reaching above 85%

--- service4/app.py
+++ service4/app.py
@@ -17,6 +17,72 @@ def get_fortune():
             return "This is going to be a year of ups and downs but in the end it will be a good one"
         elif animal == "Monkey" and number < 45:
             return "This will be a hard year but will make you stronger"
+        elif animal == "Rooster" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Rooster" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Rooster" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Dog" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Dog" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Dog" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Pig" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Pig" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Pig" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Rat" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Rat" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Rat" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Ox" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Ox" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Ox" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Tiger" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Tiger" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Tiger" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Rabbit" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Rabbit" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Rabbit" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Dragon" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Dragon" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Dragon" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Snake" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Snake" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Snake" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Horse" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Horse" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Horse" and number < 45:
+            return "This will be a hard year but will make you stronger"
+        elif animal == "Sheep" and number > 75:
+            return "This is going to be a great year in which all of your dreams come true"
+        elif animal == "Sheep" and number < 75 and number > 45:
+            return "This is going to be a year of ups and downs but in the end it will be a good one"
+        elif animal == "Sheep" and number < 45:
+            return "This will be a hard year but will make you stronger"
         else:
             return "No fortune detected, you must live in another timezone"
         
--- service4/tests/test_service4.py
+++ service4/tests/test_service4.py
@@ -16,3 +16,181 @@ class TestResponse(TestBase):
                 json=dict(name = "Monkey", luck_number = 55),
             )
             self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rooster", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dog", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Pig", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rat", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Ox", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Tiger", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dragon", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rabbit", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Horse", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Snake", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Sheep", luck_number = 55),
+            )
+            self.assertIn(b'This is going to be a year of ups and downs but in the end it will be a good one', response.data)
+
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Monkey", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rooster", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dog", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Pig", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rat", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Ox", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Tiger", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dragon", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rabbit", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Horse", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Snake", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Sheep", luck_number = 77),
+            )
+            self.assertIn(b'This is going to be a great year in which all of your dreams come true', response.data)
+            
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Monkey", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rooster", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dog", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Pig", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rat", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Ox", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Tiger", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Dragon", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Rabbit", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Horse", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Snake", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            response = self.client.post(
+                url_for('get_fortune'),
+                json=dict(name = "Sheep", luck_number = 22),
+            )
+            self.assertIn(b'This will be a hard year but will make you stronger', response.data)
+            
